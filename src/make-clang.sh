a15ed163f11fb878d2562dc42d1f538948363003
entityfx <artem.solopiy@gmail.com>
2020-01-25 01:35:33 +0300

    added clang compiler support

---
+++ src/make-clang.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+echo "Make Dhrystone v 2.1"
+
+clang dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/clang/dhrystone_clang -O3    -D options="\"clang optimized\"" 
+
+echo "Make Whetstone"
+
+clang whets.c cpuidc.c -lm -lrt -o ../bin/linux/clang/whetstone_clang  -O3   -D opt="\"clang optimized (ffast)\""
+
+
+echo "Make Memspeed"
+
+clang memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/clang/memspeed_clang -O3    -D options="\"clang optimized (ffast)\""
+
+
+
+echo "Make Linpack"
+
+clang linpack.c cpuidc.c -lm -lrt -o ../bin/linux/clang/linpack_clang -O3    -D options="\"clang optimized (ffast)\""
+
+
+echo "Make Lloops"
+
+clang lloops.c cpuidc.c -lm -lrt -o ../bin/linux/clang/lloops_clang -O3    -D options="\"clang optimized (ffast)\""
+
