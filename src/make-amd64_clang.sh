e812704767d9b22485225c0f4d57c552b7d96fea
SolopiyA <artem.solopiy@gmail.com>
2020-02-06 16:17:01 +0300

    Updated build scripts

--- src/make-amd64_clang.sh
+++ src/make-amd64_clang.sh
@@ -4,58 +4,58 @@ mkdir "../bin/linux/amd64_clang"
 
 echo -e "Compile Dhrystone v 2.1\n"
 
-clang dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/amd64_clang/dhrystone_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/dhrystone_amd64_clang-__O2
-clang dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/amd64_clang/dhrystone_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/dhrystone_amd64_clang-__O3
+clang dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/amd64_clang/dhrystone_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/dhrystone_amd64_clang-x86-64_O2
+clang dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/amd64_clang/dhrystone_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/dhrystone_amd64_clang-x86-64_O3
 
 echo -e "Compile Whetstone\n"
 
-clang whets.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/whetstone_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/whetstone_amd64_clang-__O2
-clang whets.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/whetstone_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/whetstone_amd64_clang-__O3
+clang whets.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/whetstone_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/whetstone_amd64_clang-x86-64_O2
+clang whets.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/whetstone_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/whetstone_amd64_clang-x86-64_O3
 
 echo -e "Compile Memspeed\n"
 
-clang memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/memspeed_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/memspeed_amd64_clang-__O2
-clang memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/memspeed_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/memspeed_amd64_clang-__O3
+clang memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/memspeed_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/memspeed_amd64_clang-x86-64_O2
+clang memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/memspeed_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/memspeed_amd64_clang-x86-64_O3
 
 echo -e "Compile Linpack\n"
 
-clang linpack.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/linpack_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/linpack_amd64_clang-__O2
-clang linpack.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/linpack_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/linpack_amd64_clang-__O3
+clang linpack.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/linpack_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/linpack_amd64_clang-x86-64_O2
+clang linpack.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/linpack_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/linpack_amd64_clang-x86-64_O3
 
 echo -e "Compile Lloops\n"
 
-clang lloops.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/lloops_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/lloops_amd64_clang-__O2
-clang lloops.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/lloops_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/lloops_amd64_clang-__O3
+clang lloops.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/lloops_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/lloops_amd64_clang-x86-64_O2
+clang lloops.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/lloops_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/lloops_amd64_clang-x86-64_O3
 
 echo -e "Compile WhetstoneMP\n"
 
-clang mp/whetsmp.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-__O2
-clang mp/whetsmp.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-__O3
+clang mp/whetsmp.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-x86-64_O2
+clang mp/whetsmp.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/whetstone_mp_amd64_clang-x86-64_O3
 
 echo -e "Compile MPMFlops\n"
 
-clang mp/mpmflops.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/mpmflops_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/mpmflops_amd64_clang-__O2
-clang mp/mpmflops.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/mpmflops_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/mpmflops_amd64_clang-__O3
+clang mp/mpmflops.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/mpmflops_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/mpmflops_amd64_clang-x86-64_O2
+clang mp/mpmflops.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64_clang/mpmflops_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/mpmflops_amd64_clang-x86-64_O3
 
 echo -e "Compile busspeedIL\n"
 
-clang busspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/busspeedIL_amd64_clang-__O2 -O2 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/busspeedIL_amd64_clang-__O2
-clang busspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/busspeedIL_amd64_clang-__O3 -O3 -march=_ -D options="\"amd64_clang _ optimized\""
-chmod +x ../bin/linux/amd64_clang/busspeedIL_amd64_clang-__O3
+clang busspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/busspeedIL_amd64_clang-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/busspeedIL_amd64_clang-x86-64_O2
+clang busspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64_clang/busspeedIL_amd64_clang-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64_clang x86-64 optimized\""
+chmod +x ../bin/linux/amd64_clang/busspeedIL_amd64_clang-x86-64_O3
 
 
