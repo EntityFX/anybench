ba082eb4b562483a03a48cbe78407f22688b941f
Vladimir Smirnov <civiloid@google.com>
2021-03-09 20:05:07 +0100

    Refactor make and run scripts and fix FTBTS on aarch64
     * Extract common logic to `common.sh`
     * Refactor common logic
     * Simplify make scripts
     * Add mm_malloc.h from `dgemm-neon` project (MIT-licensed mm_malloc-compatible header for platforms where gcc doesn't provide native version)

---
+++ src/common.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# List of files to compile, with binary specific flags
+declare -A binaryCompileOptions
+binaryCompileOptions["drystone"]="dhry_1.c dhry_2.c cpuidc.c"
+binaryCompileOptions["whetstone"]="whets.c cpuidc.c -lm -lrt"
+binaryCompileOptions["memspeed"]="memspeed.c cpuidc.c -lm -lrt"
+binaryCompileOptions["linpack"]="linpack.c cpuidc.c -lm -lrt"
+binaryCompileOptions["lloops"]="lloops.c cpuidc.c -lm -lrt"
+binaryCompileOptions["whetstonemp"]="mp/whetsmp.c mp/cpuidc64.c -pthread -lm -lrt "
+binaryCompileOptions["mpmflops"]="mp/mpmflops.c mp/cpuidc64.c -pthread -lm -lrt"
+binaryCompileOptions["busspeedil"]="busspeed.c cpuidc.c -lm -lrt"
+
+current_arch=$(uname -m)
+
+optFlags="-O2 -O3 -Ofast"
+
+# Those variables must be filled in architecture-dependant scripts
+# compile_binary function will iterate over all variants of flags specified by targetToFlags
+declare -A targetToFlags
+declare -A targetToFPU
+
+output_dir="../bin/linux/${current_arch}"
+mkdir -p ${output_dir}
+
+compile_binary() {
+    ARCH=${1}
+    BINARY=${2}
+    SUFFIX=${3}
+    CC=${4:-cc}
+    echo -e "Compiling ${BINARY} for ${ARCH} (${current_arch})\n"
+    FPUS="${targetToFPU[${ARCH}]}"
+    MTUNES="${targetToFlags[${ARCH}]}"
+    for FPU in "" ${FPUS}; do
+        for MTUNE in "" ${MTUNES}; do
+            EXTRA_CFLAGS="${OPT} ${MTUNE} ${FPU}"
+            for OPT in ${optFlags}; do
+                BINARY_NAME="${BINARY}_${SUFFIX}-${ARCH}${OPT//-/_}${FPU//-mfpu=/_}$(sed 's/^-m[a-z]\+=//' <<< ${MTUNE})"
+                ${CC} ${binaryCompileOptions[${BINARY}]} -o ${output_dir}/${BINARY_NAME} -march=${ARCH} ${EXTRA_CFLAGS} -D options="\"${current_arch} ${ARCH} optimized\""
+                chmod +x ${output_dir}/${BINARY_NAME}
+            done
+        done
+    done
+    echo
+}
--- src/make-amd64.sh
+++ src/make-amd64.sh
@@ -1,77 +1,12 @@
-#!/bin/bash
-mkdir "../bin/linux/"
-mkdir "../bin/linux/amd64"
+# Include common functions and operations
+source common.sh
 
-echo -e "Compile Dhrystone v 2.1\n"
-
-cc dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/amd64/dhrystone_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/dhrystone_amd64-x86-64_O2
-cc dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/amd64/dhrystone_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/dhrystone_amd64-x86-64_O3
-cc dhry_1.c dhry_2.c cpuidc.c -o ../bin/linux/amd64/dhrystone_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/dhrystone_amd64-x86-64_Ofast
-
-echo -e "Compile Whetstone\n"
-
-cc whets.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/whetstone_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/whetstone_amd64-x86-64_O2
-cc whets.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/whetstone_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/whetstone_amd64-x86-64_O3
-cc whets.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/whetstone_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/whetstone_amd64-x86-64_Ofast
-
-echo -e "Compile Memspeed\n"
-
-cc memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/memspeed_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/memspeed_amd64-x86-64_O2
-cc memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/memspeed_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/memspeed_amd64-x86-64_O3
-cc memspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/memspeed_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/memspeed_amd64-x86-64_Ofast
-
-echo -e "Compile Linpack\n"
-
-cc linpack.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/linpack_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/linpack_amd64-x86-64_O2
-cc linpack.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/linpack_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/linpack_amd64-x86-64_O3
-cc linpack.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/linpack_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/linpack_amd64-x86-64_Ofast
-
-echo -e "Compile Lloops\n"
-
-cc lloops.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/lloops_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/lloops_amd64-x86-64_O2
-cc lloops.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/lloops_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/lloops_amd64-x86-64_O3
-cc lloops.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/lloops_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/lloops_amd64-x86-64_Ofast
-
-echo -e "Compile WhetstoneMP\n"
-
-cc mp/whetsmp.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64/whetstone_mp_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/whetstone_mp_amd64-x86-64_O2
-cc mp/whetsmp.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64/whetstone_mp_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/whetstone_mp_amd64-x86-64_O3
-cc mp/whetsmp.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64/whetstone_mp_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/whetstone_mp_amd64-x86-64_Ofast
-
-echo -e "Compile MPMFlops\n"
-
-cc mp/mpmflops.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64/mpmflops_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/mpmflops_amd64-x86-64_O2
-cc mp/mpmflops.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64/mpmflops_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/mpmflops_amd64-x86-64_O3
-cc mp/mpmflops.c mp/cpuidc64.c -m64 -pthread -lm -lrt -o ../bin/linux/amd64/mpmflops_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/mpmflops_amd64-x86-64_Ofast
-
-echo -e "Compile busspeedIL\n"
-
-cc busspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/busspeedIL_amd64-x86-64_O2 -O2 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/busspeedIL_amd64-x86-64_O2
-cc busspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/busspeedIL_amd64-x86-64_O3 -O3 -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/busspeedIL_amd64-x86-64_O3
-cc busspeed.c cpuidc.c -lm -lrt -o ../bin/linux/amd64/busspeedIL_amd64-x86-64_Ofast -Ofast -march=x86-64 -D options="\"amd64 x86-64 optimized\""
-chmod +x ../bin/linux/amd64/busspeedIL_amd64-x86-64_Ofast
+targetToFlags["x86-64"]="-march=znver2"
+targetToFPU["x86-64"]=""
 
 
+for BINARY in "${!binaryCompileOptions[@]}"; do
+    for ARCH in "${!targetToFlags[@]}"; do
+        compile_binary ${ARCH} ${BINARY} amd64
+    done
+done
