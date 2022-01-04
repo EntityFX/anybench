8b9e88b34ce2191fb99bf19f5492bf37944c7e75
Vladimir Smirnov <civil.over@gmail.com>
2022-01-04 23:38:05 +0100

    Add OS Info script for Linux
      * Add simple script that grabs some information about CPU, memory, etc (everything that doesn't require root privileges)

--- src/run.sh
+++ src/run.sh
@@ -5,12 +5,24 @@ RESULT_DIR="../results/${current_arch}"
 mkdir -p "${RESULT_DIR}"
 
 if [[ ${os_name} == "mac" ]]; then
-	./cpu_info_mac.sh
+    echo "Will gather system information"
+    ./cpu_info_mac.sh
+fi
+
+if [[ ${os_name} == "Linux" ]]; then
+    echo "Will gather system information"
+    ./cpu_info_linux.sh
 fi
 
 BIN_DIR="../bin/${os_name}/${current_arch}"
-for BINARY in $(ls ${BIN_DIR}/); do
+BINARY_LIST="$(ls ${BIN_DIR}/)"
+if [[ "${#@}" -eq 1 ]]; then
+	BINARY=${1}
+	BINARY_LIST="$(ls ${BIN_DIR}/ | grep ${BINARY})"
+fi
+for BINARY in ${BINARY_LIST}; do
     [[ ! -x "${BIN_DIR}/${BINARY}" ]] && continue ||:
     echo "Running ${BINARY}"
-    "${BIN_DIR}/${BINARY}" &> "${RESULT_DIR}/${BINARY}.stdout_stderr.log" 
+    BASE_BINARY_NAME="$(cut -d_ -f 1 <<< ${BINARY})"
+    "${BIN_DIR}/${BINARY}" ${binaryExtraArgs[${BASE_BINARY_NAME}]} &> "${RESULT_DIR}/${BINARY}.stdout_stderr.log" 
 done
